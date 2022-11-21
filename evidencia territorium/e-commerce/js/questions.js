cf9965dcc736d0ba9796dba8b8ec4b502eceb293
SOFTWARE <SOFTWARE@YOPDFRCN10F922.FormacionSena.RED>
2022-11-21 07:58:45 -0500

    senasd

---
+++ evidencia territorium/e-commerce/js/questions.js
@@ -0,0 +1,20 @@
+(function(){
+    const titleQuestions = [...document.querySelectorAll('.questions_title')];
+
+    titleQuestions.forEach(questions =>{
+        questions.addEventListener('click', () => {
+            let height = 0;
+            let answer = questions.nextElementSibling;
+            let addPadding = questions.parentElement.parentElement;
+
+            addPadding.classList.toggle('questions_padding--add');
+            questions.children[0].classList.toggle('question_arrow--rotate');
+
+            if(answer.clientHeight === 0){
+                height = answer.scrollHeight; 
+            }
+            answer.style.height = `${height}px`;
+        })
+    })
+    
+})()
